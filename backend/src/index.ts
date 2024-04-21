77db038ed73145f0e1b6b945293e2e5c5721371e
hectorberryg <hectorberryg@gmail.com>
2024-04-22 00:23:18 +0200

    Cleaned code. Added CMC call endpoint. Better error handling added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,21 @@
-import * as express from "express"
-import * as bodyParser from "body-parser"
-import { AppDataSource } from "./data-source"
-import MainAPIRouter from "./routes/routes"
-import { Token } from "./entity/Token"
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { AppDataSource } from "./data-source";
+import MainAPIRouter from "./routes/routes";
 
 AppDataSource.initialize().then(async () => {
 
     // create express app
-    const app = express()
-    app.use(bodyParser.json())
+    const app = express();
+    app.use(bodyParser.json());
 
     app.use(MainAPIRouter);
 
     // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
     // start express server
-    app.listen(3000)
+    app.listen(3000);
 
-    console.log("Express server has started on port 3000. Open http://localhost:3000/ to see results")
+    console.log("Express server has started on port 3000. Open http://localhost:3000/ to see results");
 
-}).catch(error => console.log(error))
+}).catch(error => console.log(error));
--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,65 +1,92 @@
-import { TokenController } from "../controller/TokenController"
+import { TokenController } from "../controller/TokenController";
 import { Request, Response, NextFunction, Router } from 'express';
 import axios from "axios";
+import tokenPriceJson from '../types/tokenPriceJson';
+import { Token } from "../entity/Token";
 
-const MainAPIRouter = Router();
-let controller: TokenController = new TokenController()
+const MainAPIRouter: Router = Router();
+const controller: TokenController = new TokenController();
+const CMC_API_KEY_HEADER: string = 'X-CMC_PRO_API_KEY';
+const LATEST_QUOTES_CMC_ENDPOINT: string = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest';
+type TokenArrayOrError = Token[] | { error: string };
+type TokenOrError = Token | { error: string };
+
+
+function getQuotesFromJSON(data: object): tokenPriceJson {
+    let newJson: tokenPriceJson = {};
+    for (const key in data) {
+        newJson[key] = {
+            CMC_id: data[key][0].id,
+            name: data[key][0].name,
+            symbol: data[key][0].symbol,
+            price: data[key][0].quote.USD.price,
+        };
+    };
+    return newJson;
+}
 
 MainAPIRouter.get('/tokens', async (req: Request, res: Response, next: NextFunction) => {
-    // Your route logic here
     try {
-        const tokens = await controller.all(req, res); // Call the controller method
-        res.status(200).json(tokens); // Send the response
+        const tokens: TokenArrayOrError = await controller.all();
+        res.status(200).json(tokens);
     } catch (error) {
-        next(error); // Pass any errors to the error handling middleware
-    }
+        next(error);
+    };
 });
 
 MainAPIRouter.get('/tokens/:tokenId', async(req: Request, res: Response, next: NextFunction) => {
     try {
-        const token = await controller.one(req, res); // Call the controller method
-        res.status(200).json(token); // Send the response
+        const token: TokenOrError = await controller.one(req);
+        res.status(200).json(token);
     } catch (error) {
-        next(error); // Pass any errors to the error handling middleware
-    }
+        next(error);
+    };
 });
 
 MainAPIRouter.post('/tokens/add', async(req: Request, res: Response, next: NextFunction) => {
     try {
-        const token = await controller.save(req, res); // Call the controller method
-        res.status(200).json(token); // Send the response
+        const token = await controller.save(req);
+        res.status(200).json(token);
     } catch (error) {
-        next(error); // Pass any errors to the error handling middleware
-    }
+        next(error);
+    };
 });
 
 MainAPIRouter.delete('/tokens/:tokenId', async(req: Request, res: Response, next: NextFunction) => {
     try {
-        const token = await controller.remove(req, res); // Call the controller method
-        res.status(200).json(token); // Send the response
+        const token = await controller.remove(req);
+        res.status(200).json(token);
     } catch (error) {
-        next(error); // Pass any errors to the error handling middleware
-    }
+        next(error);
+    };
 });
 
 MainAPIRouter.post('/tokens/getPrice', async(req: Request, res: Response, next: NextFunction) => {
     try {
-        const tokens: Array<Number> = req.body.tokens // Call the controller method
+        const tokens: Array<string> = req.body.symbol;
         const headers = {
-            'X-CMC_PRO_API_KEY': 'Custom Value',
+            [CMC_API_KEY_HEADER]: process.env.CMC_API_KEY,
+        };
+        const params = {
+            symbol: tokens.join(',')
         };
 
-        const response = await axios.get(' https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
-            headers: headers
+        const response = await axios.get(LATEST_QUOTES_CMC_ENDPOINT, {
+            headers,
+            params
         });
 
-        const data = response.data;
-        
+        let quotes: tokenPriceJson = getQuotesFromJSON(response.data.data);
 
-        res.json(data);
+        res.status(200).json(quotes);
     } catch (error) {
-        next(error); // Pass any errors to the error handling middleware
-    }
+        next(error);
+    };
+});
+
+MainAPIRouter.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
 });
 
 export default MainAPIRouter;
