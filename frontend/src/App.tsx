292d7183f0e390de7d0d951d7a0e786f8bd08101
hectorberryg <hectorberryg@gmail.com>
2024-04-22 10:07:21 +0200

    WIP. Error Component added. Loading svg added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -40,7 +40,9 @@ function App() {
       <p className='text-center bg-gray-400'>Powered by CoinMarketCap</p>
       <div className="flex flex-col min-h-screen bg-gray-100">
         {loading ? (
-          <img src='./tube-spinner.svg' className="w-8 h-8"/>
+          <div className="flex justify-center items-start pt-14 h-screen">
+            <img src='./tube-spinner.svg' className="w-12 h-12" />
+          </div>
         ) : error ? (
           <ErrorComponent error={error} />
         ) : (
