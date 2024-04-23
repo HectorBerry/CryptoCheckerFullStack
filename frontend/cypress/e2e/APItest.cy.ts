4d2a9594cce04b415bec36afb7b520fb58a4e21f
hectorberryg <hectorberryg@gmail.com>
2024-04-23 06:33:18 +0200

    API cypress tests added

---
+++ frontend/cypress/e2e/APItest.cy.ts
@@ -0,0 +1,35 @@
+describe('API test suite', () => {
+  it('Get price data from API', () => {
+    cy.fixture('tokenPrices.json').then((availableTokens) => {
+      cy.request('POST', 'http://localhost:8888/tokens/getPrice', availableTokens).then((response) => {
+        if (response) {
+          const responseBody = response.body;
+          expect(response.status).to.equal(200);
+          expect(responseBody).to.have.property('BTC');
+        };
+      });
+    });
+  });
+
+  it('Get available tokens from API', () => {
+    cy.request('GET', 'http://localhost:8888/tokens').then((response) => {
+      if (response) {
+        const responseBody = response.body;
+        expect(response.status).to.equal(200)
+        responseBody.forEach((item: Object) => {
+          expect(item).to.have.property('symbol')
+        });
+      };
+    });
+  });
+
+  it('Get token by ID from API', () => {
+    cy.request('GET', 'http://localhost:8888/tokens/1').then((response) => {
+      if (response) {
+        const responseBody = response.body;
+        expect(response.status).to.equal(200);
+        expect(responseBody).to.have.property('symbol');
+      };
+    });
+  });
+});
