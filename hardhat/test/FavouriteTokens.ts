2fe66621a93c4a5c77d11776d409d654eddd7b26
hectorberryg <hectorberryg@gmail.com>
2024-04-23 16:10:39 +0200

    Smart contract updated to include delete and checks, tests updated

--- hardhat/test/FavouriteTokens.ts
+++ hardhat/test/FavouriteTokens.ts
@@ -17,7 +17,15 @@ describe("FavouriteTokens", function () {
   it("should set favourite token", async function () {
     await favouriteTokens.connect(addr1).setFavouriteToken("ETH");
     const tokens = await favouriteTokens.connect(addr1).getFavouriteTokens();
-    expect(tokens).to.eql(["ETH"]); // Fixing the assertion to expect ["ETH"]
+    expect(tokens).to.eql(["ETH"]);
+  });
+
+  it("if token is there already, it should not set favourite token", async function () {
+    const tokenString = "ETH"
+    await favouriteTokens.connect(addr1).setFavouriteToken(tokenString);
+    await expect(favouriteTokens.connect(addr1).setFavouriteToken(tokenString)).to.be.revertedWith(
+      "Token already in favourites"
+    )
   });
 
   it("should return favourite tokens", async function () {
@@ -25,7 +33,21 @@ describe("FavouriteTokens", function () {
     await favouriteTokens.connect(addr1).setFavouriteToken("ETH");
 
     const tokens = await favouriteTokens.connect(addr1).getFavouriteTokens();
-    expect(tokens).to.eql(["BTC", "ETH"]); // Fixing the assertion to expect ["BTC", "ETH"]
+    expect(tokens).to.eql(["BTC", "ETH"]);
+  });
+
+  it("if token is not in favourites, it should revert delete operation", async function () {
+    await expect(favouriteTokens.connect(addr1).deleteFavouriteToken("NOTTHERE")).to.be.revertedWith(
+      "Token is not in favourites"
+    )
+  });
+
+  it("should delete specified favourite token", async function () {
+    const tokenString = "BTC"
+    await favouriteTokens.connect(addr1).setFavouriteToken(tokenString);
+    await favouriteTokens.connect(addr1).deleteFavouriteToken(tokenString);
+    const tokens = await favouriteTokens.getFavouriteTokens();
+    expect(tokens).to.eql([]);
   });
 
   it("should return empty array if no favourite tokens set", async function () {
